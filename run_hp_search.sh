78a0cb99091dad20fb3ee7c4c2e53cf4f50a99b5
acostacos <acostacarlom@gmail.com>
2025-07-09 01:52:31 +0800

    Add config files for hyperparameter search

--- run_hp_search.sh
+++ run_hp_search.sh
@@ -9,13 +9,13 @@
 . venv/bin/activate
 
 # Global Mass Loss hyperparameter search
-srun python hyperparameter_search.py --config 'configs/global_loss_config.yaml' --model 'GAT'
+srun python hyperparameter_search.py --config 'configs/hp_search_global_config.yaml' --model 'GAT'
 
 # Local Mass Loss hyperparameter search
-srun python hyperparameter_search.py --config 'configs/local_loss_config.yaml' --model 'GAT'
+srun python hyperparameter_search.py --config 'configs/hp_search_local_config.yaml' --model 'GAT'
 
 # Global and Local Mass Loss hyperparameter search
-srun python hyperparameter_search.py --config 'configs/config.yaml' --model 'GAT'
+srun python hyperparameter_search.py --config 'configs/hp_search_config.yaml' --model 'GAT'
 
 # Base NodeEdge GNN hyperparameter search
-srun python hyperparameter_search.py --config 'configs/no_physics_config.yaml' --model 'NodeEdgeGNN'
+srun python hyperparameter_search.py --config 'configs/hp_search_no_physics_config.yaml' --model 'NodeEdgeGNN'

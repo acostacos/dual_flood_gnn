a0d40c1cf80cd9f4b612e0702582189fb897ce76
acostacos <acostacarlom@gmail.com>
2025-07-12 15:07:43 +0800

    Restore hyperparameters; Add config for fixed edges

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -129,7 +129,7 @@ def main():
             LOCAL_LOSS_PERCENTS = [0.0003, 0.0002, 0.0001, 0.00009, 0.00008]
             HYPERPARAMETERS['local_mass_loss_percent'] = LOCAL_LOSS_PERCENTS
         if use_edge_pred_loss:
-            EDGE_LOSS_PERCENTS = [0.2, 0.25, 0.3, 0.35, 0.4]
+            EDGE_LOSS_PERCENTS = [0.1, 0.3, 0.5, 0.7, 0.9]
             HYPERPARAMETERS['edge_pred_loss_percent'] = EDGE_LOSS_PERCENTS
 
         hyperparameter_list = list(HYPERPARAMETERS.keys())

c24338cb15bc94be45d673028b5b50663a901ba5
acostacos <acostacarlom@gmail.com>
2025-07-30 16:08:53 +0800

    Add experiment details autoregressive training

--- experiment.sh
+++ experiment.sh
@@ -8,20 +8,12 @@
 
 . venv/bin/activate
 
-srun python train.py --config 'configs/experiment_num_layers/config_1layer.yaml' --model 'NodeEdgeGNNAttn'
-srun python train.py --config 'configs/experiment_num_layers/config_2layer.yaml' --model 'NodeEdgeGNNAttn'
-srun python train.py --config 'configs/experiment_num_layers/config_3layer.yaml' --model 'NodeEdgeGNNAttn'
-srun python train.py --config 'configs/experiment_num_layers/config_4layer.yaml' --model 'NodeEdgeGNNAttn'
-srun python train.py --config 'configs/experiment_num_layers/config_6layer.yaml' --model 'NodeEdgeGNNAttn'
-srun python train.py --config 'configs/experiment_num_layers/config_8layer.yaml' --model 'NodeEdgeGNNAttn'
-srun python train.py --config 'configs/experiment_num_layers/config_10layer.yaml' --model 'NodeEdgeGNNAttn'
-
-# srun python train.py --config 'configs/experiment_auto/config_supervised.yaml' --model 'NodeEdgeGNNAttn'
-# srun python train.py --config 'configs/experiment_auto/config_autoregressive_2_step.yaml' --model 'NodeEdgeGNNAttn'
-# srun python train.py --config 'configs/experiment_auto/config_autoregressive_3_step.yaml' --model 'NodeEdgeGNNAttn'
-# srun python train.py --config 'configs/experiment_auto/config_autoregressive_4_step.yaml' --model 'NodeEdgeGNNAttn'
-# srun python train.py --config 'configs/experiment_auto/config_autoregressive_6_step.yaml' --model 'NodeEdgeGNNAttn'
-# srun python train.py --config 'configs/experiment_auto/config_autoregressive_8_step.yaml' --model 'NodeEdgeGNNAttn'
+srun python train.py --config 'configs/experiment_auto/config_supervised.yaml' --model 'NodeEdgeGNNAttn'
+srun python train.py --config 'configs/experiment_auto/config_autoregressive_2_step.yaml' --model 'NodeEdgeGNNAttn'
+srun python train.py --config 'configs/experiment_auto/config_autoregressive_3_step.yaml' --model 'NodeEdgeGNNAttn'
+srun python train.py --config 'configs/experiment_auto/config_autoregressive_4_step.yaml' --model 'NodeEdgeGNNAttn'
+srun python train.py --config 'configs/experiment_auto/config_autoregressive_6_step.yaml' --model 'NodeEdgeGNNAttn'
+srun python train.py --config 'configs/experiment_auto/config_autoregressive_8_step.yaml' --model 'NodeEdgeGNNAttn'
 
 # srun python train.py --config 'configs/experiment/base_config.yaml' --model 'NodeEdgeGNNAttn'
 # srun python train.py --config 'configs/experiment/global_loss_config.yaml' --model 'NodeEdgeGNNAttn'

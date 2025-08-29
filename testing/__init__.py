a8bcdb97c418ab0abbd8fa38975c3adc2138d15d
acostacos <acostacarlom@gmail.com>
2025-08-29 11:32:18 +0800

    Add Regression tester for edge models

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,6 +1,7 @@
 from .dual_autoregressive_tester import DualAutoregressiveTester
 from .dual_regression_tester import DualRegressionTester
 from .edge_autoregressive_tester import EdgeAutoregressiveTester
+from .edge_regression_tester import EdgeRegressionTester
 from .node_autoregressive_tester import NodeAutoregressiveTester
 from .node_regression_tester import NodeRegressionTester
 
@@ -8,6 +9,7 @@ __all__ = [
     'DualAutoregressiveTester',
     'DualRegressionTester',
     'EdgeAutoregressiveTester',
+    'EdgeRegressionTester',
     'NodeAutoregressiveTester',
-    'NodeRegressionTester'
+    'NodeRegressionTester',
 ]
---
+++ testing/edge_regression_tester.py
@@ -0,0 +1,56 @@
+import torch
+
+from torch_geometric.loader import DataLoader
+from utils.validation_stats import ValidationStats
+
+from .base_tester import BaseTester
+
+class EdgeRegressionTester(BaseTester):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def test(self):
+        for event_idx, run_id in enumerate(self.dataset.hec_ras_run_ids):
+            self.log(f'Validating on run {event_idx + 1}/{len(self.dataset.hec_ras_run_ids)} with Run ID {run_id}')
+
+            validation_stats = ValidationStats(logger=self.logger,
+                                                previous_timesteps=self.dataset.previous_timesteps,
+                                                normalizer=self.dataset.normalizer,
+                                                is_normalized=self.dataset.is_normalized,
+                                                delta_t=self.dataset.timestep_interval)
+            self.run_test_for_event(event_idx, validation_stats)
+            validation_stats.print_stats_summary()
+            self.events_validation_stats.append(validation_stats)
+
+        self.log(f'Average Edge RMSE across events: {self.get_avg_edge_rmse():.4e}')
+
+    def run_test_for_event(self, event_idx: int, validation_stats: ValidationStats):
+        validation_stats.start_validate()
+        self.model.eval()
+        with torch.no_grad():
+            event_start_idx = self.dataset.event_start_idx[event_idx] + self.rollout_start
+            event_end_idx = self.dataset.event_start_idx[event_idx + 1] if event_idx + 1 < len(self.dataset.event_start_idx) else self.dataset.total_rollout_timesteps
+            if self.rollout_timesteps is not None:
+                event_end_idx = event_start_idx + self.rollout_timesteps
+                assert event_end_idx <= (self.dataset.event_start_idx[event_idx + 1] if event_idx + 1 < len(self.dataset.event_start_idx) else self.dataset.total_rollout_timesteps), \
+                    f'Event end index {event_end_idx} exceeds dataset length {self.dataset.total_rollout_timesteps} for event_idx {event_idx}.'
+            event_dataset = self.dataset[event_start_idx:event_end_idx]
+            dataloader = DataLoader(event_dataset, batch_size=1, shuffle=False) # Enforce batch size = 1 for autoregressive testing
+
+            for graph in dataloader:
+                graph = graph.to(self.device)
+
+                x, edge_index, edge_attr = graph.x, graph.edge_index, graph.edge_attr
+                edge_pred = self.model(x, edge_index, edge_attr)
+
+                # Override boundary conditions in predictions
+                # Only override inflow edges as outflow edges are predicted by the model
+                edge_pred[self.inflow_edges_mask] = graph.y_edge[self.inflow_edges_mask]
+
+                label_edge = graph.y_edge
+                if self.dataset.is_normalized:
+                    edge_pred = self.dataset.normalizer.denormalize(self.dataset.EDGE_TARGET_FEATURE, edge_pred)
+                    label_edge = self.dataset.normalizer.denormalize(self.dataset.EDGE_TARGET_FEATURE, label_edge)
+
+                validation_stats.update_edge_stats_for_timestep(edge_pred.cpu(), label_edge.cpu())
+        validation_stats.end_validate()

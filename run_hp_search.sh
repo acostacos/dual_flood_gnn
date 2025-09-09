b9c5cb3ca14e00cbefe33d022b5abe3c64105407
acostacos <acostacarlom@gmail.com>
2025-09-09 22:50:19 +0800

    Update config files for edge pred hyperparameter search

--- run_hp_search.sh
+++ run_hp_search.sh
@@ -4,32 +4,9 @@
 #SBATCH --partition=gpu-long
 #SBATCH --gpus=a100-40:1
 #SBATCH --mem-per-cpu=128000
-#SBATCH --time=2880
+#SBATCH --time=5760
 
 . venv/bin/activate
 
-# Bayesian Search
 # Base NodeEdge GNN hyperparameter search
-srun python bayesian_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' --summary_file 'train.csv' --num_trials 20 --num_folds 5
-
-# Global Mass Loss hyperparameter search
-srun python bayesian_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' 'global_mass_loss' --summary_file 'train.csv' --num_trials 20 --num_folds 5
-
-# Local Mass Loss hyperparameter search
-srun python bayesian_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' 'local_mass_loss' --summary_file 'train.csv' --num_trials 20 --num_folds 5
-
-# Global and Local Mass Loss hyperparameter search
-srun python bayesian_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' 'global_mass_loss' 'local_mass_loss' --summary_file 'train.csv' --num_trials 20 --num_folds 5
-
-# # Grid Search
-# # Base NodeEdge GNN hyperparameter search
-# srun python grid_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' --summary_file 'train.csv'
-
-# # Global Mass Loss hyperparameter search
-# srun python grid_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' 'global_mass_loss' --summary_file 'train.csv'
-
-# # Local Mass Loss hyperparameter search
-# srun python grid_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' 'local_mass_loss' --summary_file 'train.csv'
-
-# # Global and Local Mass Loss hyperparameter search
-# srun python grid_search.py --config 'configs/hp_search_config.yaml' --model 'NodeEdgeGNNAttn' --hyperparameters 'edge_pred_loss' 'global_mass_loss' 'local_mass_loss' --summary_file 'train.csv'
+srun python hp_search.py --config 'configs/no_physics_config.yaml' --hparam_config 'configs/hparam_config/edge_pred_loss.yaml' --model 'NodeEdgeGNNAttn'

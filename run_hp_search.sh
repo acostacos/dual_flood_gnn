a350a1dbb36cd2bab3d0435da19d5c97c414a9c6
acostacos <acostacarlom@gmail.com>
2025-09-20 10:38:44 +0800

    Update run_hp_search script

--- run_hp_search.sh
+++ run_hp_search.sh
@@ -9,10 +9,13 @@
 . venv/bin/activate
 
 # Base NodeEdge GNN hyperparameter search
-srun python hp_search.py --config 'configs/no_physics_config.yaml' --hparam_config 'configs/hparam_config/edge_pred_loss.yaml' --model 'NodeEdgeGNNAttn'
+srun python hp_search.py --config 'configs/no_physics_config.yaml' --hparam_config 'configs/hparam_config/edge_pred_loss.yaml' --model 'NodeEdgeGNN'
 
 # Global Mass Conservation Loss hyperparameter search
-srun python hp_search.py --config 'configs/global_loss_config.yaml' --hparam_config 'configs/hparam_config/global_mass_loss.yaml' --model 'NodeEdgeGNNAttn'
+srun python hp_search.py --config 'configs/global_loss_config.yaml' --hparam_config 'configs/hparam_config/global_mass_loss.yaml' --model 'NodeEdgeGNN'
 
 # Local Mass Conservation Loss hyperparameter search
-srun python hp_search.py --config 'configs/local_loss_config.yaml' --hparam_config 'configs/hparam_config/local_mass_loss.yaml' --model 'NodeEdgeGNNAttn'
+srun python hp_search.py --config 'configs/local_loss_config.yaml' --hparam_config 'configs/hparam_config/local_mass_loss.yaml' --model 'NodeEdgeGNN'
+
+# Global and Local Mass Conservation Loss hyperparameter search
+srun python hp_search.py --config 'configs/config.yaml' --hparam_config 'configs/hparam_config/global_and_local_mass_loss.yaml' --model 'NodeEdgeGNN'

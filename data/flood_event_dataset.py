2b435b33ddf17289b822aae52ee841c70010209f
acostacos <acostacarlom@gmail.com>
2026-01-06 10:18:12 +0800

    Remove position dependent features for mSWEGNN Dataset

--- data/flood_event_dataset.py
+++ data/flood_event_dataset.py
@@ -21,9 +21,9 @@ from .dataset_normalizer import DatasetNormalizer
 
 class FloodEventDataset(Dataset):
     EVENT_FILE_KEYS = ['Simulation_Filepath', 'Nodes_Shp_Filepath', 'Edges_Shp_Filepath', 'DEM_Filepath']
-    STATIC_NODE_FEATURES = ['position_x', 'position_y', 'area', 'roughness', 'elevation', 'aspect', 'curvature', 'flow_accumulation']
+    STATIC_NODE_FEATURES = ['area', 'roughness', 'elevation', 'aspect', 'curvature', 'flow_accumulation']
     DYNAMIC_NODE_FEATURES = ['inflow', 'rainfall', 'water_volume'] # Not included: 'water_depth'
-    STATIC_EDGE_FEATURES = ['relative_position_x', 'relative_position_y', 'face_length', 'length', 'slope']
+    STATIC_EDGE_FEATURES = ['face_length', 'length', 'slope']
     DYNAMIC_EDGE_FEATURES = ['face_flow'] # Not included: 'velocity'
     NODE_TARGET_FEATURE = 'water_volume'
     EDGE_TARGET_FEATURE = 'face_flow'

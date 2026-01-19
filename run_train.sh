a14a61b9df4aa9481872551d3cf4304c43c17616
acostacos <acostacarlom@gmail.com>
2026-01-19 21:52:23 +0800

    Adjust physics loss weights based on HP search

--- run_train.sh
+++ run_train.sh
@@ -9,19 +9,5 @@
 . venv/bin/activate
 
 # DUALFloodGNN
-# Base Architecture
-srun python train.py --config 'configs/config.yaml' --model 'DUALFloodGNN'
-
-# Physics-Informed Variants
-srun python train.py --config 'configs/global_loss_config.yaml' --model 'DUALFloodGNN'
-srun python train.py --config 'configs/local_loss_config.yaml' --model 'DUALFloodGNN'
-srun python train.py --config 'configs/no_physics_config.yaml' --model 'DUALFloodGNN'
-
-# Standard GNN Architectures
-# Node Prediction
-srun python train.py --config 'configs/standard_gnn_config.yaml' --model 'GAT'
-srun python train.py --config 'configs/standard_gnn_config.yaml' --model 'GCN'
-
-# Edge Prediction
-srun python train.py --config 'configs/standard_gnn_config.yaml' --model 'EdgeGAT'
-srun python train.py --config 'configs/standard_gnn_config.yaml' --model 'EdgeGCN'
+srun python train.py --config 'configs/mswegnn_config.yaml' --model 'DUALFloodGNN' --seed 666
+srun python train.py --config 'configs/mswegnn_no_physics_config.yaml' --model 'DUALFloodGNN' --seed 666
